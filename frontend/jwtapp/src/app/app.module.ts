0e59ed16b2bea537a8da387c00364b46441ad46f
Nisha Yadav <nishayadav1703@gmail.com>
2025-04-18 11:39:04 +0530

    Frontend Changes : created component for home, nav, login, register page and added a parameter for tsconfig file

--- frontend/jwtapp/src/app/app.module.ts
+++ frontend/jwtapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './component/home/home.component';
+import { NavComponent } from './component/nav/nav.component';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
